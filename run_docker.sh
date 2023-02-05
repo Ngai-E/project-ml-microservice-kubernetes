e71b2059eb15832a0fe2cbe08b9980c41da3b8fa
EC2 Default User <ec2-user@ip-172-31-6-63.ec2.internal>
2023-02-05 11:35:23 +0000

    complete docker push script

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Build image
-docker build --tag=api .
+docker build --tag=ngaie/ml-microservice-kubernetes .
 
 # List docker images
 docker image ls
 
 # Run flask app
-docker run -p 8000:80 api
+docker run -p 8000:80 ml-microservice-kubernetes
