ab43a2329167467ace6b564b007965df54cdf3d8
Ngai Elizabeth <asobingai@gmail.com>
2023-02-05 23:45:27 +0100

    update files

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,9 @@ COPY model_data  /app/model_data/
 RUN pip install --upgrade pip &&\
     pip install --trusted-host pypi.python.org -r requirements.txt
 
+RUN wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 &&\
+    chmod +x /bin/hadolint    
+
 EXPOSE 80
 
 CMD [ "python", "app.py" ]
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=ngaie/ml-microservice-kubernetes:latest
+dockerpath=ngaie/ml-microservice-kubernetes
 
 kubectl create deploy ml-microservice-kubernetes --image=$dockerpath
 
 
 kubectl get deploy,rs,svc,pods
 
-kubectl port-forward pod/ml-microservice-kubernetes-6bd579f6d9-8cncz --address 0.0.0.0 8000:80
+kubectl port-forward pod/ml-microservice-kubernetes-78679f6d56-44zsx --address 0.0.0.0 8000:80
 
